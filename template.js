c75fb655d70623c0680bdffde097ace3c2de2eb1
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-06-23 17:34:17 -0300

    agrego video

---
+++ template.js
@@ -0,0 +1,25 @@
+/**************/
+/***TEMPLATE***/
+/**************/
+document.getElementById("resultTempERR").style.visibility = "hidden";
+document.getElementById("resultTempCOR").style.visibility = "hidden";
+
+document.getElementById("correctTemp").style.visibility = "hidden";
+document.getElementById("errorTemp").style.visibility = "hidden";
+
+function obtenerEntradaTemp() {
+
+    var entry = document.getElementById("form").value; 
+    
+        if (entry == "mariadelrosario") {
+            document.getElementById("resultTempCOR").style.visibility = "visible";
+            document.getElementById("correctTemp").style.visibility = "visible";
+        } else {
+            document.getElementById("resultTempERR").style.visibility = "visible";
+            document.getElementById("errorTemp").style.visibility = "visible";
+        }
+}
+
+
+
+
