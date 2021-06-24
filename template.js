d143c3cd58274d249e785234eb64ed76671dc41d
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-06-24 14:56:27 -0300

    prueba cel11

--- template.js
+++ template.js
@@ -1,21 +1,22 @@
 /**************/
 /***TEMPLATE***/
 /**************/
-document.getElementById("resultTempERR").style.visibility = "hidden";
-document.getElementById("resultTempCOR").style.visibility = "hidden";
 
-document.getElementById("correctTemp").style.visibility = "hidden";
-document.getElementById("errorTemp").style.visibility = "hidden";
+document.getElementById("resultTempERR").style.display="none";
+document.getElementById("resultTempCOR").style.display="none";
+
+document.getElementById("correctTemp").style.display="none";
+document.getElementById("errorTemp").style.display="none";
 
 function obtenerEntradaTemp() {
 
     var entry = document.getElementById("form").value; 
     
         if (entry == "mariadelrosario") {
-            document.getElementById("resultTempCOR").style.visibility = "visible";
-            document.getElementById("correctTemp").style.visibility = "visible";
+            document.getElementById("resultTempCOR").style.display="block";
+            document.getElementById("correctTemp").style.display="block";
         } else {
-            document.getElementById("resultTempERR").style.visibility = "visible";
-            document.getElementById("errorTemp").style.visibility = "visible";
+            document.getElementById("resultTempERR").style.display="block";
+            document.getElementById("errorTemp").style.display="block";
         }
 }
