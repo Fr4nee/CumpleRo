c596fdc08cde6fcc4d6c2b7d3d16f85e836ff91e
Fr4nee <franromano99@outlook.com>
2021-07-15 00:28:34 -0300

    cami

---
+++ template.js
@@ -0,0 +1,34 @@
+/**************/
+/***TEMPLATE***/
+/**************/
+
+document.getElementById("resultTempERR").style.display="none";
+document.getElementById("resultTempCOR").style.display="none";
+
+document.getElementById("correctTemp").style.display="none";
+document.getElementById("errorTemp").style.display="none";
+
+function obtenerEntradaTemp(pass) {
+
+    var entry = document.getElementById("form").value; 
+    
+        if (entry == pass) {
+            document.getElementById("resultTempCOR").style.display="block";
+            document.getElementById("correctTemp").style.display="block";
+        } else {
+            document.getElementById("resultTempERR").style.display="block";
+            document.getElementById("errorTemp").style.display="block";
+        }
+}
+
+function obtenerEntradaYo(pass) {
+
+    var entry = document.getElementById("form").value; 
+    
+        if (entry == pass) {
+            location.href="yo.html";
+        } else {
+            document.getElementById("resultTempERR").style.display="block";
+            document.getElementById("errorTemp").style.display="block";
+        }
+}
